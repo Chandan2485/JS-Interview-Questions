8d0019c0bc6e7b3f02d7d4792fd9f6ddb6e32672
Chandan2485 <chandan96kar@gmail.com>
2022-10-29 19:43:10 +0530

    test

---
+++ test.js
@@ -0,0 +1,22 @@
+// console.log( isNaN('hello'));
+// console.log(Number.isNaN('hello'));
+//  isNaN('hello')
+
+// let arr = ["a", "b", "c"];
+// for (let key in arr) {
+//   console.log(key);
+// }
+// for (let value of arr) {
+//   console.log(value);
+// }
+// console.log(num)
+// var num=7;
+func()
+
+// function func(){
+//     console.log('10')
+// }
+
+const func =()=>{
+    console.log('10')
+}
